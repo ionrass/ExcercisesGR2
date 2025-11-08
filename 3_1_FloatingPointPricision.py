e2e8bd59795147509329da84a6f0f931719645f4
SPECTRE-RAS\34642 <Ion.RasSischin@stud.srh-university.de>
2025-11-08 14:49:59 +0100

    Cylinder Volume

---
+++ 3_1_FloatingPointPricision.py
@@ -0,0 +1,5 @@
+num1 = 0.1
+num = 0.2
+result = num1 + num
+print("Result of adding 0.1 and 0.2:", result)
+# Demonstrating floating-point precision issue
---
+++ 3_2CylinderVolume.py
@@ -0,0 +1,5 @@
+# Calculate cylinder volume
+import math
+def cylinder_volume(radius, height):
+    volume = math.pi * (radius**2) * height
+    return volume
