d56988a37d19226d381684f364921b17979233f4
SPECTRE-RAS\34642 <Ion.RasSischin@stud.srh-university.de>
2025-11-08 15:12:26 +0100

    User Age

---
+++ 3_3-UserAge.py
@@ -0,0 +1,5 @@
+# Use comparison and logical operators (and, or, not) to check if a user's age is between 18 and 65, inclusive, but not exactly 30.
+age = int(input("Enter your age: "))
+is_between_18_and_65 = age >= 18 and age <= 65
+is_not_30 = age != 30
+print("Is the user's age between 18 and 65, inclusive, but not exactly 30?", is_between_18_and_65 and is_not_30)
